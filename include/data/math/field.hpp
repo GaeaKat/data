9b3599471bd1b4d8e19dd31149194e2f9f92842f
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2019-01-07 08:44:32 -0600

    Math types for algebra and complex numbers.

---
+++ include/data/math/field.hpp
@@ -0,0 +1,19 @@
+#ifndef DATA_MATH_FIELD_HPP
+#define DATA_MATH_FIELD_HPP
+
+#include "ring.hpp"
+
+namespace data {
+    
+    namespace math {
+            
+        template <typename x>
+        struct field : public ring<x> {
+            x divide(x a, x b) const {
+                return a / b;
+            }
+        };
+    
+    }
+    
+}
